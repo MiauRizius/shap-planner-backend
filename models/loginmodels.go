2f4e0bb8ce39c92c37e3ab2b637aaaf3141662dc
Maurice L. <maurice.lariviere@icloud.com>
2026-03-03 20:07:35 +0100

    Updated refresh-token property names

--- models/loginmodels.go
+++ models/loginmodels.go
@@ -2,7 +2,7 @@ package models
 
 type RefreshToken struct {
 	ID         string `json:"id"`
-	UserID     string `json:"userid"`
+	UserID     string `json:"user_id"`
 	Token      string `json:"token"`
 	ExpiresAt  int64  `json:"expires_at"`
 	CreatedAt  int64  `json:"created_at"`
